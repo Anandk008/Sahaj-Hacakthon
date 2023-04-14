25f3c3fa8351ee39a49ddf99bd885a533e302abb
Anandk008 <Anandkadale1@gmail.com>
2023-04-14 14:31:40 +0530

    Second done

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const http = require("http");
+var id = 1;
 
 const server = http.createServer(async (req, res) => {
   if (req.url === "/greeting" && req.method === "GET") {
@@ -7,12 +8,27 @@ const server = http.createServer(async (req, res) => {
     res.end();
   }
 
+  if(req.url=== "/employee" && req.method === "POST"){
+    // var body = '';
+    // req.on('data' , chunk => {
+    //   body += chunk.toString();
+    // })
+    // req.on('end', () => {
+    //   console.log(body);
+    //   req.end()
+    // })
+      id++;
+    res.writeHead(201, {'Content-Type' : "application/json"});
+    res.write("employeeId : ");
+    res.write(id.toString() );
+    res.end();
+  }
+
   else {
     res.writeHead(404, {"Content-Type": "application/json"});
     res.end(JSON.stringify({message: "Route note found"}));
   }
 });
-
 
 
 server.listen(8080, () => console.log("Server Runing on 8080"));
